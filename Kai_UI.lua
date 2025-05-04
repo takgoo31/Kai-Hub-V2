135bdb2412af3d66a0d8f20f188e4125cf296f2a
takgoo31 <takgoo.official@gmail.com>
2025-05-04 20:33:38 +0800

    Create Kai_UI.lua

---
+++ Kai_UI.lua
@@ -0,0 +1,20 @@
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
+
+
+Button_Icon = "rbxassetid://137592432303285"
+              
+
+local Window = Fluent:CreateWindow({
+    Title = "Kai Hub | Test",
+    SubTitle = "by: Kai Team",
+    TabWidth = 150,
+    Size = UDim2.fromOffset(400, 340),
+    Acrylic = false,
+    Theme = "Dark"
+})
+
+
+local Main = Window:AddTab({
+    Title = "Main",
+    Icon = "home"
+})
